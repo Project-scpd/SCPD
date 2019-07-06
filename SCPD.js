eb4276b018f3b7ee17230e525a016055776d5d95
Project-scpd <52592734+Project-scpd@users.noreply.github.com>
2019-07-06 14:38:07 +0800

    SCPD.js
    er, hold, still usles

---
+++ SCPD.js
@@ -0,0 +1,46 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+	typeof define === 'function' && define.amd ? define(['exports'], factory) :
+	(global = global || self, factory(global.SCPD = {}));
+}(this, function (exports) {'use strict';
+	//POLYFILLS, SETUPS AND CUSTOMS
+	if ( Math.sign === undefined ) {
+		// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/sign
+		Math.sign = function ( x ) {
+			return ( x < 0 ) ? - 1 : ( x > 0 ) ? 1 : + x;
+		};
+	}
+	if ( 'name' in Function.prototype === false ) {
+		// Missing in IE
+		// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/name
+		Object.defineProperty( Function.prototype, 'name', {
+			get: function () {
+				return this.toString().match( /^\s*function\s*([^\(\s]*)/ )[ 1 ];
+			}
+		} );
+	}
+	if ( Object.assign === undefined ) {
+		// Missing in IE
+		// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
+		( function () {
+			Object.assign = function ( target ) {
+				if ( target === undefined || target === null ) {
+					throw new TypeError( 'Cannot convert undefined or null to object' );
+				}
+				var output = Object( target );
+				for ( var index = 1; index < arguments.length; index ++ ) {
+					var source = arguments[ index ];
+					if ( source !== undefined && source !== null ) {
+						for ( var nextKey in source ) {
+							if ( Object.prototype.hasOwnProperty.call( source, nextKey ) ) {
+								output[ nextKey ] = source[ nextKey ];
+							}
+						}
+					}
+				}
+				return output;
+			};
+		} )();
+	}
+
+}));
